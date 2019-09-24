72ccfcdd4648d1d5bc3cfcec29351c40d7b94836
AliceCheney <Cheneyjie@qq.com>
2019-09-24 18:46:59 +0800

    2019/9/24/18:46

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log("adf")
+import '../css/index.css'
+import './index.html'
+const $ = require('jquery');
+require('select2');
+require('select2/dist/css/select2.min.css');
+const iziToast = require('izitoast');
+require('izitoast/dist/css/iziToast.min.css');
+require('bootstrap/dist/css/bootstrap.min.css')
